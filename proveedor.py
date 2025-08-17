7fee679d0b18b2f29a27bdddc3ae0b50bc5bfd6c
Geovanny Alcon <151603150+g4usSi@users.noreply.github.com>
2025-08-17 14:07:50 -0600

    Estructura general del proyecto

---
+++ proveedor.py
@@ -0,0 +1,13 @@
+class Proveedor:
+    def __init__(self, nombre, direccion, telefono):
+        self.nombre = nombre
+        self.direccion = direccion
+        self.telefono = telefono
+
+    def __str__(self):
+        return f"Proveedor: {self.nombre}, Dirección: {self.direccion}, Teléfono: {self.telefono}"
+    
+    def __eq__(self, other):
+        if isinstance(other, Proveedor):
+            return self.nombre == other.nombre and self.direccion == other.direccion and self.telefono == other.telefono
+        return False
