d30177bc4bc137efbb9f2567d22638edc85f1da1
Zaid <henryddiaz696@gmail.com>
2025-08-18 15:35:23 -0600

    arreglando detalles del menu

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from proveedor import Proveedor
 from arbolB import ArbolB
-
+#funcion que inserta datos iniciales en el arbol
 def insertar_datos(arbol):
    
-    proveedor1 = Proveedor("Miku", "1", "Electricista", "5")
-    proveedor2 = Proveedor("Teto", "2", "Plomera", "4")
-    proveedor3 = Proveedor("Jimbo", "3", "Carpintero", "3")
-    proveedor4 = Proveedor("Lucho", "4", "Pintor", "2")
-    proveedor5 = Proveedor("Marta", "5", "Jardinera", "5")
-    proveedor6 = Proveedor("Ana", "6", "Soldador", "4") 
-    proveedor7 = Proveedor("Lucho", "7", "Dentista", "2")
-    proveedor8 = Proveedor("Marta", "8", "Piloto", "5")
-    proveedor9 = Proveedor("Ana", "9", "Doctor", "4")
-    proveedor10 = Proveedor("Goku", "10", "Programador", "4")  
+    proveedor1 = Proveedor("Miku", "1", "electricista", "5")
+    proveedor2 = Proveedor("Teto", "2", "plomera", "4")
+    proveedor3 = Proveedor("Jimbo", "3", "carpintero", "3")
+    proveedor4 = Proveedor("Lucho", "4", "pintor", "2")
+    proveedor5 = Proveedor("Marta", "5", "jardinera", "5")
+    proveedor6 = Proveedor("Ana", "6", "soldador", "4") 
+    proveedor7 = Proveedor("Lucho", "7", "dentista", "2")
+    proveedor8 = Proveedor("Celia", "8", "piloto", "5")
+    proveedor9 = Proveedor("Alicia", "9", "doctor", "4")
+    proveedor10 = Proveedor("Goku", "10", "programador", "4")  
 
     arbol.insertar(proveedor1)
     arbol.insertar(proveedor2)
@@ -24,7 +24,7 @@ def insertar_datos(arbol):
     arbol.insertar(proveedor8)
     arbol.insertar(proveedor9)
     arbol.insertar(proveedor10)
-
+#funcion que se encarga de validar las entradas de numeros
 def pedir_numero():
     while True:
         try:
@@ -32,7 +32,7 @@ def pedir_numero():
             return numero  
         except ValueError:
             print("Error: debe ingresar un número válido. Inténtelo de nuevo.")
-
+#funcion que retorna la seleccion de la profesion
 def retornar_profesion(numero):
 
         if numero < 1 or numero > 13:
@@ -65,21 +65,23 @@ def retornar_profesion(numero):
             return "Arquitecto"
         else:
             return None   
-    
+#prints de todas las profesiones 
 def mostrar_profesiones():
     print("Profesiones disponibles:")
-    print("[ 1 ] Electricista")
-    print("[ 2 ] Plomera")
-    print("[ 3 ] Carpintero")
-    print("[ 4 ] Pintor")
-    print("[ 5 ] Jardinera")
-    print("[ 6 ] Soldador")
-    print("[ 7 ] Dentista")
-    print("[ 8 ] Piloto")
-    print("[ 9 ] Doctor")
-    print("[ 10 ] Programador")
+    print("[ 1 ] electricista")
+    print("[ 2 ] plomera")
+    print("[ 3 ] carpintero")
+    print("[ 4 ] pintor")
+    print("[ 5 ] jardinera")
+    print("[ 6 ] soldador")
+    print("[ 7 ] dentista")
+    print("[ 8 ] piloto")
+    print("[ 9 ] doctor")
+    print("[ 10 ] programador")
+    print("[ 11 ] abogado")
+    print("[ 12 ] arquitecto")
     print("ingresa el numero de la profesión que desea agregar al proveedor:")
-
+#print del menu principal
 def menu_principal():
     print(" ---------------------------------------- ")
     print(" BIENVENIDO A SERVICIOS LOCALES S.A. ")
@@ -87,20 +89,22 @@ def menu_principal():
     print("[ 1 ] INSERTAR UN NUEVO PROVEEDOR")
     print("[ 2 ] BUSCAR PROVEEDORES POR SERVICIO")
     print("[ 3 ] VISUALIZAR PROVEEDORES")
-    print("[ 4 ] SALIR")
+    print("[ 4 ] recorrido con forme al orden del arbol")
+    print("[ 5 ] SALIR")
 
 def main():
-    # Crear instancia del árbol B
-    tamano_orden = int(input("Ingrese el orden del árbol B (minimo 2): "))
+    # Crear instancia del arbol B
+    print("Ingrese el tamaño de orden del arbol B (debe ser mayor o igual a 2):")
+    tamano_orden = pedir_numero()#solicitamos al usuario ingresar el tamaño del orden del arbol
     arbol = ArbolB(tamano_orden)
     # Insertar datos de proveedores
-    insertar_datos(arbol)
-
+    insertar_datos(arbol)#llama la funcion que ingresa los datos iniciales
+    #inicio del menu principal
     while True:
         menu_principal()
-        opcion = input("Seleccione una opción: ")
-
-        if opcion == "1":
+        opcion = pedir_numero()
+        #Opcion 1 del menu, insertar un nuevo proveedor
+        if opcion == 1:
             nombre = input("Ingrese el nombre del proveedor: ")
             id = input("Ingrese el ID del proveedor: ")
             mostrar_profesiones()
@@ -116,6 +120,7 @@ def main():
 
             while True:
                 try:
+                    print("Ingrese la calificación del proveedor (1 a 5):")
                     calificacion = pedir_numero()
                     if 1 <= calificacion <= 5:
                         break
@@ -127,44 +132,49 @@ def main():
             nuevo_proveedor = Proveedor(nombre, id, servicio, calificacion)
             arbol.insertar(nuevo_proveedor)
             print(f"Proveedor: {nombre}, que brinda el servicio de: {servicio} ha sido insertado correctamente.")
-
-        elif opcion == "2":
+        #opcion 2 del menu, buscar proveedores por servicio
+        elif opcion == 2:
             servicio_buscar = input("Ingrese el tipo de servicio a buscar: ")
             proveedores = arbol.buscar_oficio(servicio_buscar) 
-            # no se si hay que cambiar esto, ya que el método buscar del árbol B busca claves, no objetos Proveedor directamente.
+            
             if proveedores is not None:
                 print(f"\nProveedores que brindan el servicio de {servicio_buscar}:")
                 for i in proveedores:
                     print(i)
             else:
                 print(f"No se encontraron proveedores que brinden el servicio de {servicio_buscar}.")
-
-        elif opcion == "3":
+        #opcion 3 del menu, visualizar un listado de los proveedores en funcion a su nombre o calificacion
+        elif opcion == 3:
             while True:
                 print('='*50)
                 print("Visualizando proveedores:")
                 print('[ 1 ] Mostrar proveedores por Nombre')
                 print('[ 2 ] Mostrar proveedores por Calificación')
                 print('[ 3 ] Salir')
-                opcion1 = input("Seleccione una opción: ")
+                print('Seleccione una opción:')
+                opcion1 = pedir_numero()
                 listed_nodes = arbol.enlistar() 
-                if opcion1 == "1":
+                if opcion1 == 1:
                     listed_nodes.sort(key=lambda x: x.nombre)  # Ordenar por nombre
                     for proveedor in listed_nodes:
                         print(proveedor)
                     # Aquí se podría implementar la lógica para mostrar proveedores por nombre
-                elif opcion1 == "2":
+                elif opcion1 == 2:
                     listed_nodes.sort(key=lambda x: x.calificacion, reverse=True)  # Ordenar por calificación descendente
                     for proveedor in listed_nodes:
                         print(proveedor)
                     # Aquí se podría implementar la lógica para mostrar proveedores por calificación
-                elif opcion1 == "3":
+                elif opcion1 == 3:
                     print("Saliendo de la visualización de proveedores...")
                     break
                 else:
                     print("Opción no valida. Intente nuevamente.")
+        #opcion 4 del menu, recorrido con forme al ID almacenado en el arbol
+        elif opcion == 4:
+            pass
+            break
 
-        elif opcion == "4":
+        elif opcion == 5:
             print("Saliendo del programa...")
             break
 
